2d32421a4dc3db7d68b02bda31ca2c56ba5365a3
irazakharchenko <zakharchenko@ucu.edu.ua>
2017-02-07 15:40:38 +0200

    add function  is_valid start it

--- Battelship.py
+++ Battelship.py
@@ -27,24 +27,37 @@ def ship_size(dat, coor):
         ssResult = 1
         sscoor = (ord(coor[0]) - ord('A'), coor[1]-1)
         i = 1
-        if (sscoor[0] > 0 and dat[sscoor[0]-1][sscoor[1]] == '*') or( sscoor[0] < 9 and dat[sscoor[0]+1][sscoor[1]] == '*'):
+        if (sscoor[0] > 0 and dat[sscoor[0]-1][sscoor[1]] == '*' or dat[sscoor[0]-1][sscoor[1]] == 'X') or \
+                ( sscoor[0] < 9 and dat[sscoor[0]+1][sscoor[1]] == '*' or dat[sscoor[0]+1][sscoor[1]] == 'X'):
 
 
-            while sscoor[0] - i > -1 and  dat[sscoor[0]-i][sscoor[1]] == '*':
+            while sscoor[0] - i > -1 and  dat[sscoor[0]-i][sscoor[1]] == '*' or dat[sscoor[0]-i][sscoor[1]] == 'X':
                 ssResult += 1
                 i += 1
-            while sscoor[0] + i < 10  and  dat[sscoor[0]+i][sscoor[1]] == '*':
+            while sscoor[0] + i < 10  and  dat[sscoor[0]+i][sscoor[1]] == '*' or  dat[sscoor[0]+i][sscoor[1]] == 'X':
                 ssResult += 1
                 i += 1
         else:
-            while sscoor[1] - i > -1 and  dat[sscoor[0]][sscoor[1]-i] == '*':
+            while sscoor[1] - i > -1 and  dat[sscoor[0]][sscoor[1]-i] == '*' or dat[sscoor[0]][sscoor[1]-i] == 'X':
                 ssResult += 1
                 i += 1
-            while sscoor[1] + i < 10  and  dat[sscoor[0]][sscoor[1]+i] == '*':
+            while sscoor[1] + i < 10  and  dat[sscoor[0]][sscoor[1]+i] == '*' or dat[sscoor[0]][sscoor[1]+i] == 'X':
                 ssResult += 1
                 i += 1
         return ssResult
     else:
         return 0
 
+def is_valid(dataiv):
+    '''
+    (list) -> (str)
+    :return:
+    '''
+    if len(dataiv) == 10 :
+        for el in dataiv:
+            if len(dataiv) != 10:
+                retursn False
+
+    else:
+        return False
 print(ship_size(read_field('field.txt'), ('C', 10)))
