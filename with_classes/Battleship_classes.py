b2d7a60ff8ad1299cf80bf6eaf2f8fc0ce0f08c9
irazakharchenko <zakharchenko@ucu.edu.ua>
2017-02-19 16:48:21 +0200

    make contour round killed ship

--- with_classes/Battleship_classes.py
+++ with_classes/Battleship_classes.py
@@ -7,8 +7,7 @@ class Ship:
     Represent ships coordinates and are they hit.
     '''
 
-    def __init__(self, bow: object = (0, 0), horizontal: object = True, length: object = -1, hit: object = False) \
-            -> object:
+    def __init__(self, bow = (0, 0), horizontal = True, length = -1, hit = False):
         '''
         Initialize direction of ship, its left up coordinate, its length and is it hit.
         :param bow: tuple
@@ -40,6 +39,12 @@ class Ship:
             if not self.__hit:
 
                 self.__hit = True
+                
+    def length_horizontal_bow(self):
+        '''
+        return length of the ship, its coordinates and is it horizontal
+        '''
+        return [self.__length, self.horizontal, list(self.bow)]
 
 class Field():
     '''
@@ -177,7 +182,7 @@ class Field():
                     # print('y = ', y, ' x= ', x, ' i=', i)
                     # print(massx)
                     table[y][x - i] = '*'
-                    self.__ships[y][x - i] = Ship((y, x - sizes), True, sizes)
+                    self.__ships[y][x - i] = Ship((y, x - sizes + 1), True, sizes)
                     massx[y].remove(x - i)
                     if y > 0 and x - i in massx[y - 1]:
                         table[y - 1][x - i] = 'n'
@@ -253,7 +258,7 @@ class Field():
                     # print('y = ', y, ' x= ', x, ' i=', i)
                     # print(massx)
                     table[y - i][x] = '*'
-                    self.__ships[y - i][x] = Ship((y - sizes, x), False, sizes)
+                    self.__ships[y - i][x] = Ship((y - sizes+1, x), False, sizes)
                     massx[y - i].remove(x)
                     if x > 0 and x - 1 in massx[y - i]:
                         table[y - i][x - 1] = 'n'
@@ -323,6 +328,85 @@ class Field():
             if (self.coor[0],self.coor[1]) not in self.shooted_ships_coor:
                 self.shooted_ships_coor.append((self.coor[0],self.coor[1]))
             self.shooted = True
+            leng, hor, fbow =  self.__ships[self.coor[0]][self.coor[1]].length_horizontal_bow()
+            fb = True
+            #fbow[0], fbow[1] = fbow[0]+1, fbow[1]-1
+            # next lines are for the moment when you hit all ship
+            if hor:
+                fb = False
+                for i in range(leng):
+                    if self.field[fbow[0]][fbow[1]+i] != 'X':
+                        fb = True
+                        break
+                if not fb:
+                    print('self.field =', self.field)
+                    print('fbow =', fbow)
+                    if fbow[0] > 0:
+                        for i in range(leng):
+                            self.field[fbow[0]-1][fbow[1]+i] = 'o'
+                            self.lFieldWithoutShips[fbow[0]-1][fbow[1]+i] = 'o'
+                    if fbow[0] < 9:
+                        for i in range(leng):
+                            self.field[fbow[0]+1][fbow[1]+i] = 'o'
+                            self.lFieldWithoutShips[fbow[0]+1][fbow[1]+i] = 'o'
+                    if fbow[1] > 0:
+                        if fbow[0] > 0:
+                            self.field[fbow[0]-1][fbow[1]-1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]-1][fbow[1]-1] = 'o'
+                        self.field[fbow[0]][fbow[1]-1] = 'o'
+                        self.lFieldWithoutShips[fbow[0]][fbow[1]-1] = 'o'
+                        if fbow[0] < 9:
+                            self.field[fbow[0]+1][fbow[1]-1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]+1][fbow[1]-1] = 'o'
+                    if fbow[1] < 9:
+                        if fbow[0] > 0:
+                            self.field[fbow[0]-1][fbow[1]+leng - 1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]-1][fbow[1]+leng-1] = 'o'
+                        self.field[fbow[0]][fbow[1]+leng-1] = 'o'
+                        self.lFieldWithoutShips[fbow[0]][fbow[1]+leng-1] = 'o'
+                        if fbow[0] < 9:
+                            self.field[fbow[0]+1][fbow[1]+1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]+1][fbow[1]+leng-1] = 'o'  
+                
+            else:
+                fb = False
+                for i in range(leng):
+                    if self.field[fbow[0]+i][fbow[1]] != 'X':
+                        fb = True
+                        break
+                #if not fb and leng != 1:
+                    #fbow[0], fbow[1] = fbow[1], fbow[0]
+                if not fb:
+                    print('self.field =', self.field)
+                    print('fbow =', fbow)
+                    if fbow[1] > 0:
+                        for i in range(leng):
+                            self.field[fbow[0]+i][fbow[1]-1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]+i][fbow[1]-1] = 'o'
+                    if fbow[1] < 9:
+                        for i in range(leng):
+                            self.field[fbow[0]+i][fbow[1]+1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]+i][fbow[1]+1]= 'o'
+                    if fbow[0] > 0:
+                        if fbow[1] > 0:
+                            self.field[fbow[0]-1][fbow[1]-1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]-1][fbow[1]-1] = 'o'
+                        self.field[fbow[0]-1][fbow[1]] = 'o'
+                        self.lFieldWithoutShips[fbow[0]-1][fbow[1]] = 'o'
+                        if fbow[1] < 9:
+                            self.field[fbow[0]-1][fbow[1]+1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]-1][fbow[1]+1] = 'o'
+                    if fbow[0] < 9:
+                        if fbow[1] > 0:
+                            self.field[fbow[0]+leng][fbow[1]-1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]+leng][fbow[1]-1] = 'o'
+                        self.field[fbow[0]+leng][fbow[1]] = 'o'
+                        self.lFieldWithoutShips[fbow[0]+leng][fbow[1]] = 'o'
+                        if fbow[1] < 9:
+                            self.field[fbow[0]+leng][fbow[1]+1] = 'o'
+                            self.lFieldWithoutShips[fbow[0]+leng][fbow[1]+1] = 'o'  
+                      
+             
             #print('yey')
             #print(self.__ships[self.coor[0]][self.coor[1]])
             #print('self.field = ', self.field)
@@ -381,10 +465,11 @@ class Player:
         '''
         rpcoor = input('Please write coordinates as A7, J5 ')
         #print(ord(rpcoor[0].lower()))
-        rpcoor = [rpcoor[0], int(rpcoor[1:])]
+        rpcoor = [rpcoor[0], rpcoor[1:]]
         if rpcoor[0].lower() not in ascii_lowercase[:10]:
             rpcoorconv = (-1, -1)
         else:
+            rpcoor[1] = int(rpcoor[1])
             rpcoorconv = (ord(rpcoor[0].lower()) - ord('a'), rpcoor[1]-1)
 
         while (rpcoorconv[0] > 9 or rpcoorconv[0] < 0 ) or (rpcoorconv[1] > 9 or rpcoorconv[1] < 0):
@@ -416,7 +501,7 @@ class Game:
         #print(self.__field[0].field_with_ships(), '\n', self.__field[1].field_with_ships())
         self.__current_player = 0
         self.shoot_coord = [(0,0), (0,0)]
-        self.mass = ' 12345678910'
+        self.mass = '  12345678910'
         
     def current_player(self):
         '''
@@ -461,7 +546,7 @@ class Game:
         print fields
         :return:
         '''
-        system('cls')
+        #system('cls')
         print('Game Battleship\n')
         print("It's {0}'s turn.\n ".format(self.name_player()))
         print("It's your field.")
@@ -469,7 +554,7 @@ class Game:
         fws = self.G_field_with_ships()
         print(self.mass)
         for i in range(10):
-            print("{0} {1}\n".format(ascii_uppercase[i], fws[i]))
+            print("{0} {1}".format(ascii_uppercase[i], fws[i]))
 
         print("It's your enemy's field.")
         self.print_enemy_field()
@@ -505,25 +590,29 @@ class Game:
             print('Good shoot.\n')
             system('cls')
         else:
+            print("You don't hurt enemy's ship.")
             self.end_turn()
-        print("It's {0}'s field".format(self.name_player()))
+
         print('Your turn is end.')
         sleep(7)
         self.print_field()
 
         self.G_read_position()
-        print('Good shoot.\n')
+        
         system('cls')
+        print('Good shoot.\n')
 
         while self.__field[self.__current_player].shooted:
             self.print_field()
 
             self.G_read_position()
-            print('Good shoot.\n')
+            
             system('cls')
+            print('Good shoot.\n')
         else:
+            print("You don't hurt enemy's ship.")
             self.end_turn()
-        print("It's {0}'s field".format(self.name_player()))
+
         print('Your turn is end.')
         sleep(7)
 
@@ -531,19 +620,22 @@ class Game:
             self.print_field()
 
             self.G_read_position()
-            print('Good shoot.\n')
+            
 
             system('cls')
+            print('Good shoot.\n')
 
             while self.__field[self.__current_player].shooted:
                 self.print_field()
 
                 self.G_read_position()
-                print('Good shoot.\n')
+                
                 system('cls')
+                print('Good shoot.\n')
             else:
+                print("You don't hurt enemy's ship.")
                 self.end_turn()
-            print("It's {0}'s field".format(self.name_player()))
+
             print('Your turn is end.')
             sleep(7)
         else:
@@ -552,22 +644,7 @@ class Game:
             print('Player {0} win.'.format(self.name_player()))
 
 
-
-
-
-'''
-n = Field()
-n.generate_field()
-
-
-n.shoot_at()
-'''
-#player1 = Player
-#print(player1.read_position())
 n = Game()
 n.start()
-        
-
-
 
 
